37f9c8826364ea5ad50ecbae0700b8b925536b41
Gasper Urh <gasper.urh@gmail.com>
2020-05-27 10:51:49 +0200

    updated function to return base64 QR code

--- aws_lambda.py
+++ aws_lambda.py
@@ -1,12 +1,20 @@
-import json
+import base64
+
+from io import BytesIO
 
 from qr_generator import generate_qr_code
 
-def lambda_handler(event, context):
 
+def lambda_handler(event, context):
     image = generate_qr_code()
+    with BytesIO() as buffer:
+        image.save(buffer, 'png')
+        base64qrCode = base64.b64encode(buffer.getvalue()).decode('utf-8')
 
     return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'source_name': event.get('name'),
+        'name': 'UPN QR code generator',
+        'author': 'urgas9',
+        'url': 'https://github.com/urgas9/py-upn-qr',
+        'qr_code': base64qrCode,
     }
--- update_aws_function.sh
+++ update_aws_function.sh
@@ -1,24 +1,25 @@
 #!/usr/bin/env bash
 
-venv_name="venv-release"
+temp_dir="temp-release"
 output_zip_name="upnqr-lambda-release.zip"
 
 # Prepare virtual environment and install required dependencies
-virtualenv -p python3 ${venv_name}
-source ${venv_name}/bin/activate
-pip install -r requirements.txt
+mkdir ${temp_dir}
+pip install -r requirements.txt -t ${temp_dir}
 deactivate
 
 # Remove possible pre-existing ZIP file
 rm ${output_zip_name}
 
 # Add dependencies to the ZIP file
-cd ${venv_name}/lib/python3.6/site-packages
+cd ${temp_dir}
 zip -r9 ${OLDPWD}/${output_zip_name} .
 
 # Add source code to the ZIP file
 cd ${OLDPWD}
 zip -g ${output_zip_name} aws_lambda.py qr_generator.py
+
+rm -r ${temp_dir}
 
 # Update AWS Lambda function
 aws lambda update-function-code --function-name upnqr --zip-file fileb://${output_zip_name} --region=eu-central-1
